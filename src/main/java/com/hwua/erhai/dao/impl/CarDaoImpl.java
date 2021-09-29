6fb05acd9a551b0c7c91de9d1073f2f6551a6041
kyaru <2324470938@qq.com>
2021-09-29 19:42:35 +0800

    todo categoryID

--- src/main/java/com/hwua/erhai/dao/impl/CarDaoImpl.java
+++ src/main/java/com/hwua/erhai/dao/impl/CarDaoImpl.java
@@ -112,8 +112,10 @@ public class CarDaoImpl extends JDBCTemplate implements ICarDao {
         String sql = "SELECT car.id, b.id, b.name, car.model, cay.id, cay.name,"
                 + "car.t_comments, car.rent,car.status, car.usable "
                 + "FROM t_car car, t_brand b, t_category cay "
-//todo
+                +"where car.brand_id = b.id AND car.category_id = cay.id "
+                +"and car.category_id = ?"
                 ;
+
         query(sql, null, new ResultSetHandler() {
             @Override
             public void handleRs(ResultSet rs) throws SQLException {
